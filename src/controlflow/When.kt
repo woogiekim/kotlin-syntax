8e91a7272695a9ef85c0067bbc5fd5e7c1f644ce
Woogie Kim <mdir2kor@gmail.com>
2021-11-21 01:23:59 +0900

    feat(control-flow): when statement 활용

---
+++ src/controlflow/When.kt
@@ -0,0 +1,38 @@
+package controlflow
+
+fun main() {
+    cases("Hello")
+    cases(1)
+    cases(0L)
+    cases(MyClass())
+    cases("hello")
+
+    println()
+
+    println(whenAssign("Hello"))
+    println(whenAssign(3.4))
+    println(whenAssign(1))
+    println(whenAssign(MyClass()))
+}
+
+fun cases(obj: Any) {
+    when (obj) {
+        1 -> println("One")
+        "Hello" -> println("Greeting")
+        is Long -> println("Long")
+        !is String -> println("Not a String")
+        else -> println("Unknown")
+    }
+}
+
+fun whenAssign(obj: Any): Any {
+    val result = when (obj) {
+        1 -> "one"
+        "Hello" -> 1
+        is Long -> false
+        else -> 42
+    }
+    return result
+}
+
+class MyClass
